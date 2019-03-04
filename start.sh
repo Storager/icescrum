6fefb9a9c5dbaa4785db25ae953cf539854d3726
Dmitry Tsirulnikov <dmitry.tsirulnikov@gmail.com>
2019-03-04 07:57:21 +0200

    Refactored due old docker

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -x
-pwd
-
-wget -c https://www.icescrum.com/downloads/v7/icescrum.jar -O icescrum.jar
-
-/usr/bin/java -jar icescrum.jar
+whoami
+ls -la $APP_DIR
+ls -la $APP_DIR/icescrum
+ls -la $APP_DIR/icescrum/logs || mkdir $APP_DIR/icescrum/logs
+ls -la $APP_DIR/icescrum/data || mkdir $APP_DIR/icescrum/data
+ls -la /tmp
+touch $APP_DIR/icescrum/logs/icescrum.logs
+/usr/bin/java -jar $APP_DIR/icescrum.jar
